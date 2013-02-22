9b302a49d72aa4c365d7a050f9d21ca980bef568
Andrew Vit <andrew@avit.ca>
2013-02-21 23:42:33 -0800

    Add failing specs for DST transition

--- spec/examples/hourly_rule_spec.rb
+++ spec/examples/hourly_rule_spec.rb
@@ -25,6 +25,27 @@ module IceCube
       dates.should == [DAY, DAY + 3 * IceCube::ONE_HOUR, DAY + 6 * IceCube::ONE_HOUR]
     end
 
+    it 'should work across DST start hour' do
+      schedule = Schedule.new(t0 = Time.local(2013, 3, 10, 1, 0, 0))
+      schedule.add_recurrence_rule Rule.hourly
+      schedule.first(3).should == [
+        Time.local(2013, 3, 10, 1, 0, 0), # -0800
+        Time.local(2013, 3, 10, 3, 0, 0), # -0700
+        Time.local(2013, 3, 10, 4, 0, 0)  # -0700
+      ]
+    end
+
+    it 'should not skip times in DST end hour' do
+      schedule = Schedule.new(t0 = Time.local(2013, 11, 3, 0, 0, 0))
+      schedule.add_recurrence_rule Rule.hourly
+      schedule.first(4).should == [
+        Time.local(2013, 11, 3, 0, 0, 0),             # -0700
+        Time.local(2013, 11, 3, 1, 0, 0) - ONE_HOUR,  # -0700
+        Time.local(2013, 11, 3, 1, 0, 0),             # -0800
+        Time.local(2013, 11, 3, 2, 0, 0),             # -0800
+      ]
+    end
+
     it 'should produce the correct days for @interval = 3' do
       start_date = DAY
       schedule = Schedule.new(start_date)
